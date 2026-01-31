49644b9f42a005afb3603ea354c81ecc54340d89
e0423017-eng <e0423017@sriher.edu.in>
2026-01-31 15:15:56 +0530

    Added start script

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const mongoose = require("mongoose")    
 
 module.exports = () => {
-    mongoose.connect("mongodb://localhost:27017/test-rms-db")
+    mongoose.connect("process.env.DB_URL")
         .then(() => { console.log("database connected") })
         .catch((err) => { console.log(err) })
 }
